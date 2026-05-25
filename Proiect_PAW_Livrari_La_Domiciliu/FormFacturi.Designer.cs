39f39a50b60012d454acb75a69fc832340eaa6ae
Catalin Dobre <dobrecatalinmarius7@gmail.com>
2026-05-25 22:56:03 +0300

    facut FormFacturi

--- Proiect_PAW_Livrari_La_Domiciliu/FormFacturi.Designer.cs
+++ Proiect_PAW_Livrari_La_Domiciliu/FormFacturi.Designer.cs
@@ -1,16 +1,9 @@
-﻿namespace Proiect_PAW_Livrari_La_Domiciliu
+namespace Proiect_PAW_Livrari_La_Domiciliu
 {
     partial class FormFacturi
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,18 +15,186 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelDateFactura = new System.Windows.Forms.Label();
+            this.labelComanda = new System.Windows.Forms.Label();
+            this.labelDataEmitere = new System.Windows.Forms.Label();
+            this.labelPretTotal = new System.Windows.Forms.Label();
+            this.cbComanda = new System.Windows.Forms.ComboBox();
+            this.dtpDataEmitere = new System.Windows.Forms.DateTimePicker();
+            this.tbPretTotal = new System.Windows.Forms.TextBox();
+            this.btnSalveaza = new System.Windows.Forms.Button();
+            this.btnAnuleaza = new System.Windows.Forms.Button();
+            this.labelListaFacturi = new System.Windows.Forms.Label();
+            this.btnAdauga = new System.Windows.Forms.Button();
+            this.btnModifica = new System.Windows.Forms.Button();
+            this.btnSterge = new System.Windows.Forms.Button();
+            this.dgvFacturi = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFacturi)).BeginInit();
+            this.SuspendLayout();
+            //
+            // labelDateFactura
+            //
+            this.labelDateFactura.AutoSize = true;
+            this.labelDateFactura.Location = new System.Drawing.Point(46, 9);
+            this.labelDateFactura.Name = "labelDateFactura";
+            this.labelDateFactura.TabIndex = 10;
+            this.labelDateFactura.Text = "DATE FACTURA";
+            //
+            // labelComanda
+            //
+            this.labelComanda.AutoSize = true;
+            this.labelComanda.Location = new System.Drawing.Point(46, 36);
+            this.labelComanda.Name = "labelComanda";
+            this.labelComanda.TabIndex = 0;
+            this.labelComanda.Text = "&Comanda";
+            //
+            // labelDataEmitere
+            //
+            this.labelDataEmitere.AutoSize = true;
+            this.labelDataEmitere.Location = new System.Drawing.Point(422, 36);
+            this.labelDataEmitere.Name = "labelDataEmitere";
+            this.labelDataEmitere.TabIndex = 1;
+            this.labelDataEmitere.Text = "&Data emitere";
+            //
+            // labelPretTotal
+            //
+            this.labelPretTotal.AutoSize = true;
+            this.labelPretTotal.Location = new System.Drawing.Point(46, 75);
+            this.labelPretTotal.Name = "labelPretTotal";
+            this.labelPretTotal.TabIndex = 2;
+            this.labelPretTotal.Text = "&Pret total";
+            //
+            // cbComanda
+            //
+            this.cbComanda.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbComanda.FormattingEnabled = true;
+            this.cbComanda.Location = new System.Drawing.Point(49, 52);
+            this.cbComanda.Name = "cbComanda";
+            this.cbComanda.Size = new System.Drawing.Size(262, 21);
+            this.cbComanda.TabIndex = 0;
+            //
+            // dtpDataEmitere
+            //
+            this.dtpDataEmitere.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpDataEmitere.CustomFormat = "dd/MM/yyyy";
+            this.dtpDataEmitere.Location = new System.Drawing.Point(425, 52);
+            this.dtpDataEmitere.Name = "dtpDataEmitere";
+            this.dtpDataEmitere.Size = new System.Drawing.Size(262, 20);
+            this.dtpDataEmitere.TabIndex = 1;
+            //
+            // tbPretTotal
+            //
+            this.tbPretTotal.Location = new System.Drawing.Point(49, 91);
+            this.tbPretTotal.Name = "tbPretTotal";
+            this.tbPretTotal.Size = new System.Drawing.Size(262, 20);
+            this.tbPretTotal.TabIndex = 2;
+            //
+            // btnSalveaza
+            //
+            this.btnSalveaza.Location = new System.Drawing.Point(49, 127);
+            this.btnSalveaza.Name = "btnSalveaza";
+            this.btnSalveaza.Size = new System.Drawing.Size(94, 31);
+            this.btnSalveaza.TabIndex = 3;
+            this.btnSalveaza.Text = "Sal&veaza";
+            this.btnSalveaza.UseVisualStyleBackColor = true;
+            //
+            // btnAnuleaza
+            //
+            this.btnAnuleaza.Location = new System.Drawing.Point(149, 127);
+            this.btnAnuleaza.Name = "btnAnuleaza";
+            this.btnAnuleaza.Size = new System.Drawing.Size(94, 31);
+            this.btnAnuleaza.TabIndex = 4;
+            this.btnAnuleaza.Text = "A&nuleaza";
+            this.btnAnuleaza.UseVisualStyleBackColor = true;
+            //
+            // labelListaFacturi
+            //
+            this.labelListaFacturi.AutoSize = true;
+            this.labelListaFacturi.Location = new System.Drawing.Point(46, 174);
+            this.labelListaFacturi.Name = "labelListaFacturi";
+            this.labelListaFacturi.TabIndex = 11;
+            this.labelListaFacturi.Text = "LISTA FACTURI";
+            //
+            // btnAdauga
+            //
+            this.btnAdauga.Location = new System.Drawing.Point(390, 169);
+            this.btnAdauga.Name = "btnAdauga";
+            this.btnAdauga.Size = new System.Drawing.Size(94, 31);
+            this.btnAdauga.TabIndex = 5;
+            this.btnAdauga.Text = "Adau&ga";
+            this.btnAdauga.UseVisualStyleBackColor = true;
+            //
+            // btnModifica
+            //
+            this.btnModifica.Location = new System.Drawing.Point(493, 169);
+            this.btnModifica.Name = "btnModifica";
+            this.btnModifica.Size = new System.Drawing.Size(94, 31);
+            this.btnModifica.TabIndex = 6;
+            this.btnModifica.Text = "&Modifica";
+            this.btnModifica.UseVisualStyleBackColor = true;
+            //
+            // btnSterge
+            //
+            this.btnSterge.Location = new System.Drawing.Point(593, 169);
+            this.btnSterge.Name = "btnSterge";
+            this.btnSterge.Size = new System.Drawing.Size(94, 31);
+            this.btnSterge.TabIndex = 7;
+            this.btnSterge.Text = "St&erge";
+            this.btnSterge.UseVisualStyleBackColor = true;
+            //
+            // dgvFacturi
+            //
+            this.dgvFacturi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFacturi.Location = new System.Drawing.Point(49, 207);
+            this.dgvFacturi.Name = "dgvFacturi";
+            this.dgvFacturi.Size = new System.Drawing.Size(638, 145);
+            this.dgvFacturi.TabIndex = 8;
+            //
+            // FormFacturi
+            //
+            this.AcceptButton = this.btnSalveaza;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnAnuleaza;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvFacturi);
+            this.Controls.Add(this.btnSterge);
+            this.Controls.Add(this.btnModifica);
+            this.Controls.Add(this.btnAdauga);
+            this.Controls.Add(this.labelListaFacturi);
+            this.Controls.Add(this.btnAnuleaza);
+            this.Controls.Add(this.btnSalveaza);
+            this.Controls.Add(this.tbPretTotal);
+            this.Controls.Add(this.dtpDataEmitere);
+            this.Controls.Add(this.cbComanda);
+            this.Controls.Add(this.labelPretTotal);
+            this.Controls.Add(this.labelDataEmitere);
+            this.Controls.Add(this.labelComanda);
+            this.Controls.Add(this.labelDateFactura);
+            this.Name = "FormFacturi";
             this.Text = "FormFacturi";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFacturi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelDateFactura;
+        private System.Windows.Forms.Label labelComanda;
+        private System.Windows.Forms.Label labelDataEmitere;
+        private System.Windows.Forms.Label labelPretTotal;
+        private System.Windows.Forms.ComboBox cbComanda;
+        private System.Windows.Forms.DateTimePicker dtpDataEmitere;
+        private System.Windows.Forms.TextBox tbPretTotal;
+        private System.Windows.Forms.Button btnSalveaza;
+        private System.Windows.Forms.Button btnAnuleaza;
+        private System.Windows.Forms.Label labelListaFacturi;
+        private System.Windows.Forms.Button btnAdauga;
+        private System.Windows.Forms.Button btnModifica;
+        private System.Windows.Forms.Button btnSterge;
+        private System.Windows.Forms.DataGridView dgvFacturi;
     }
 }
